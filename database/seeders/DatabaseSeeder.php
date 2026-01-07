fd63eafac4da79786d3d816b2109672b470484a7
whyyouadty <wahyugit987@gmail.com>
2026-01-07 08:46:09 +0800

    Add ViolationTypeSeeder to DatabaseSeeder for auto-seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ClassSeeder::class,
             StudentSeeder::class,
             SettingSeeder::class,
+            ViolationTypeSeeder::class,
         ]);
     }
 }
